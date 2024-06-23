333c194cb7f9c5a1044ac4024a8a8c4a60fe4131
mine00clone <mine@clone.co.jp>
2024-06-24 05:54:19 +0900

    wagmi.ts now receives connectors from connectors.ts

--- app/src/lib/wagmi.ts
+++ app/src/lib/wagmi.ts
@@ -1,9 +1,12 @@
+import { connectors } from "./connectors";
 import { http, createConfig } from "@wagmi/core";
 import { baseSepolia } from "@wagmi/core/chains";
 
 export const wagmiConfig = createConfig({
+  connectors: connectors,
   chains: [baseSepolia],
   transports: {
     [baseSepolia.id]: http(),
   },
+  ssr: true,
 });
