89dd9ce60be5812ac6b48dadff50ab6832e97856
taijusanagi <taijusanagi@gmail.com>
2024-06-23 14:32:04 +0700

    added subgraph

--- subgraph/src/eis.ts
+++ subgraph/src/eis.ts
@@ -1,104 +1,112 @@
+import { Bytes } from "@graphprotocol/graph-ts";
+
 import {
   ApprovalForAll as ApprovalForAllEvent,
   Created as CreatedEvent,
   TransferBatch as TransferBatchEvent,
   TransferSingle as TransferSingleEvent,
-  URI as URIEvent
-} from "../generated/EIS/EIS"
+  URI as URIEvent,
+} from "../generated/EIS/EIS";
 import {
   ApprovalForAll,
   Created,
   TransferBatch,
   TransferSingle,
-  URI
-} from "../generated/schema"
+  URI,
+} from "../generated/schema";
 
 export function handleApprovalForAll(event: ApprovalForAllEvent): void {
   let entity = new ApprovalForAll(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.account = event.params.account
-  entity.operator = event.params.operator
-  entity.approved = event.params.approved
+  );
+  entity.account = event.params.account;
+  entity.operator = event.params.operator;
+  entity.approved = event.params.approved;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleCreated(event: CreatedEvent): void {
   let entity = new Created(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.tokenId = event.params.tokenId
-  entity.creator = event.params.creator
-  entity.record_creator = event.params.record.creator
-  entity.record_split = event.params.record.split
-  entity.record_name = event.params.record.name
-  entity.record_description = event.params.record.description
-  entity.record_mimeType = event.params.record.mimeType
-  entity.record_imageChunks = event.params.record.imageChunks
-  entity.record_referenceTokenIds = event.params.record.referenceTokenIds
+  );
+  entity.tokenId = event.params.tokenId;
+  entity.creator = event.params.creator;
+  entity.record_creator = event.params.record.creator;
+  entity.record_split = event.params.record.split;
+  entity.record_name = event.params.record.name;
+  entity.record_description = event.params.record.description;
+  entity.record_mimeType = event.params.record.mimeType;
+  entity.record_imageChunks = event.params.record.imageChunks.map<Bytes>(
+    (value: Bytes) => value
+  );
+  entity.record_referenceTokenIds = event.params.record.referenceTokenIds;
   entity.record_splitParams_recipients =
-    event.params.record.splitParams.recipients
+    event.params.record.splitParams.recipients.map<Bytes>(
+      (value: Bytes) => value
+    );
   entity.record_splitParams_allocations =
-    event.params.record.splitParams.allocations
+    event.params.record.splitParams.allocations;
   entity.record_splitParams_totalAllocation =
-    event.params.record.splitParams.totalAllocation
+    event.params.record.splitParams.totalAllocation;
   entity.record_splitParams_distributionIncentive =
-    event.params.record.splitParams.distributionIncentive
+    event.params.record.splitParams.distributionIncentive;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleTransferBatch(event: TransferBatchEvent): void {
   let entity = new TransferBatch(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.operator = event.params.operator
-  entity.from = event.params.from
-  entity.to = event.params.to
-  entity.ids = event.params.ids
-  entity.values = event.params.values
+  );
+  entity.operator = event.params.operator;
+  entity.from = event.params.from;
+  entity.to = event.params.to;
+  entity.ids = event.params.ids;
+  entity.values = event.params.values;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleTransferSingle(event: TransferSingleEvent): void {
   let entity = new TransferSingle(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.operator = event.params.operator
-  entity.from = event.params.from
-  entity.to = event.params.to
-  entity.EIS_id = event.params.id
-  entity.value = event.params.value
+  );
+  entity.operator = event.params.operator;
+  entity.from = event.params.from;
+  entity.to = event.params.to;
+  entity.EIS_id = event.params.id;
+  entity.value = event.params.value;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleURI(event: URIEvent): void {
-  let entity = new URI(event.transaction.hash.concatI32(event.logIndex.toI32()))
-  entity.value = event.params.value
-  entity.EIS_id = event.params.id
+  let entity = new URI(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  );
+  entity.value = event.params.value;
+  entity.EIS_id = event.params.id;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
