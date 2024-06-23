4d8cb7d2ad6d48a9237a0edbc6379d6659d4dbde
taijusanagi <taijusanagi@gmail.com>
2024-06-23 15:32:00 +0700

    modify subgraph

--- subgraph/src/eis.ts
+++ subgraph/src/eis.ts
@@ -9,5 +9,22 @@ export function handleCreated(event: CreatedEvent): void {
   let contract = ESIContract.bind(event.address);
   entity.creator = event.params.creator;
   entity.uri = contract.uri(event.params.tokenId);
+  entity.referTo = event.params.record.referenceTokenIds.map<string>((value) =>
+    value.toString()
+  );
+  entity.referedFrom = [];
+
+  let referenceTokenIds = event.params.record.referenceTokenIds;
+  for (let i = 0; i < referenceTokenIds.length; i++) {
+    let referencedEntity = Record.load(referenceTokenIds[i].toString());
+    if (referencedEntity) {
+      let referedFrom = referencedEntity.referedFrom || [];
+      referedFrom.push(event.params.tokenId.toString());
+      referedFrom.sort();
+      referencedEntity.referedFrom = referedFrom;
+      referencedEntity.save();
+    }
+  }
+
   entity.save();
 }
