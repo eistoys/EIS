219bfb958d4a61e034cbce765c9a48a32cf12505
taijusanagi <taijusanagi@gmail.com>
2024-07-10 20:14:18 +0900

    add create button in header

--- app/src/app/campaign/based-hanabi/_components/Header.tsx
+++ app/src/app/campaign/based-hanabi/_components/Header.tsx
@@ -2,8 +2,11 @@
 
 import Link from "next/link";
 import { ConnectWalletButton } from "./ConnectWalletButton";
+import { usePathname } from "next/navigation";
 
 export const Header = () => {
+  const pathname = usePathname();
+
   return (
     <div className="w-full flex justify-between items-center p-3 border-b border-[#888888] h-12 md:h-[60px]">
       <Link href="/campaign/based-hanabi">
@@ -13,7 +16,19 @@ export const Header = () => {
           className="h-6 md:h-10 cursor-pointer"
         />
       </Link>
-      <ConnectWalletButton />
+      <div className="space-x-3 flex">
+        {!pathname.includes("create") && (
+          <Link href="/campaign/based-hanabi/artworks/create">
+            <button
+              type="button"
+              className="flex justify-center items-center text-center px-4 h-6 md:h-8 text-[#191D88] bg-[#FFD582] font-bold rounded-full hover:opacity-75 tracking-wider"
+            >
+              CREATE
+            </button>
+          </Link>
+        )}
+        <ConnectWalletButton />
+      </div>
     </div>
   );
 };
