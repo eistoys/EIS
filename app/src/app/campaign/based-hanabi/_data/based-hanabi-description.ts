4a2cd47eced58508c73e45664894f5a9b4d8e3b4
taijusanagi <taijusanagi@gmail.com>
2024-07-11 10:31:07 +0900

    adjust frontend

---
+++ app/src/app/campaign/based-hanabi/_data/based-hanabi-description.ts
@@ -0,0 +1,35 @@
+export const basedHanabiDescription = `
+
+# Description
+
+## Contest Overview
+
+🚀 We’re thrilled to announce a contest using Ethereum Image Service (EIS) for our upcoming crypto meetup, Based Hanabi! 🌟 We want you to create sticker designs that will be given out at the event. Through this contest, let’s empower the Based Hanabi event and the culture of Base even more!
+This event is entirely run by volunteers and relies heavily on community support. Your participation and creativity are what make it possible.
+
+## Stickers
+
+The top 5 artworks with the most mints will be made into stickers and distributed at Based Hanabi IRL.
+
+## Minting Fees
+
+- 10% goes to EIS
+- 45% covers event expenses
+- 45% goes to the creators
+  Your contributions help sustain our community and make this event a success!
+
+## What is EIS?
+
+Ethereum Image Service (EIS) is a groundbreaking platform that allows you to create and share CC0, fully on-chain artworks. It's all about public goods and infinite remixability! With EIS, your art becomes part of a collaborative canvas in the sky, where anyone can remix and create. Plus, you can earn revenue through NFTs!
+
+## What is Hanabi?
+
+Hanabi means "fireworks" in Japanese, symbolizing the explosive creativity and vibrant community spirit we want to celebrate.
+
+## Contest Details:
+
+- ***Canvas Size:*** 64x64 pixels
+- ***Contest Period:*** 2024/7/15 - 7/22
+- ***Original Images:*** We’ll provide 4 images for you to remix
+- ***Submission:*** Remix these images into awesome pixel art using EIS!
+  `;
