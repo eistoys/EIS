f86159887416b6b1d1b30a2c546a5386880ed9e7
taijusanagi <taijusanagi@gmail.com>
2024-07-11 14:51:32 +0900

    added ogp base

--- app/src/app/campaign/based-hanabi/layout.tsx
+++ app/src/app/campaign/based-hanabi/layout.tsx
@@ -21,8 +21,8 @@ export const metadata: Metadata = {
     images: [
       {
         url: "https://eis.toys/assets/ogp-hanabi.png",
-        width: 800,
-        height: 420,
+        width: 1200,
+        height: 630,
         alt: "Ethereum Image Service",
       },
     ],
